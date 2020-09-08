2b95eb075a5f7b9dd33d4f83429c45a6e69055e7
jandersen747 <jonandersenofficial@hotmail.com>
2020-09-08 12:25:32 +0200

    masterbranch

---
+++ isPrime.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+'use strict'
+
+let x = Number(prompt("Enter value", "0"));
+
+let start = new Date();
+let b = isPrime(x);
+let stop = new Date();
+let elapsed = stop - start;
---
+++ library.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+'use strict'
+
+const isPrime = function (arg) {
+    for (var i = 2; i < arg; i += 1) {
+        if (arg % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
